1a289b9ae31ee4f485f0c904d093a1db96eac13e
Toby Padilla <tobypadilla@gmail.com>
2009-11-14 19:25:45 -0800

    added playdar.js

--- js/playgrub.js
+++ js/playgrub.js
@@ -1,5 +1,5 @@
 Playgrub = {
-    PGHOST: 'http://www.playgrub.com/',
+    PGHOST: 'http://localhost:8080/',
     VERSION: '0.7',
     playlist: {},
     client: {},
@@ -12,6 +12,26 @@ Playgrub = {
         new Playgrub.Scraper();
         new Playgrub.Client();
         new Playgrub.Bookmarklet();
+
+        /* First, set the client up with some callbacks. */
+        Playdar.setupClient({
+
+            // Called when the browser is authorised to query Playdar.
+            onAuth: function () {
+                // At this point, we can query the Playdar API for a track and start polling for matches.
+                Playdar.client.resolve("", "");
+            },
+
+            // Called in response to each poll with the results so far.
+            onResults: function (response, lastPoll) {
+                console.log('Polling ' + response.qid);
+                if (lastPoll) {
+                    // Take a look at the final response.
+                    console.dir(response);
+                }
+            }
+        });
+
     }
 };
 
@@ -22,6 +42,9 @@ Playgrub.Events = {
         Playgrub.playlist.url = window.location;
         Playgrub.playlist.title = document.title;
         Playgrub.client.write_playlist(Playgrub.playlist);
+
+        /* The client is now ready to check in with Playdar. */
+        Playdar.client.go();
     },
 
     noScraper: function() {
@@ -217,6 +240,10 @@ Playgrub.Util = {
         return "http://spiffdar.org/?spiff="+encodeURIComponent(Playgrub.PGHOST+Playgrub.playlist.id)+".xspf";
     }
 };
+
+// load Playdar
+Playgrub.Util.inject_script(Playgrub.PGHOST+'js/playdar_compressed.js');
+alert(Playgrub.PGHOST+'js/playdar_compressed.js');
 
 // load jquery - will run Playgrub.init() when done
 Playgrub.Util.load_jquery();
