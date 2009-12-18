d0fba26878de4932302b5a303801a7acc3b58e3a
Toby Padilla <tobypadilla@gmail.com>
2009-12-18 10:37:56 -0800

    add playlick scraper file

---
+++ scrapers/playlick.com.js
@@ -0,0 +1,23 @@
+/**
+ * Playlick Playgrub.source
+ * Created by: Toby Padilla <tobypadilla AT gmail DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on any Playlick setlist
+ */
+
+Playgrub.source.url = 'http://.*playlick\.com.*/.*';
+Playgrub.source.error = 'Please check your Playlick playlist.';
+Playgrub.source.scrape = function() {
+    var title = $("#playlistMetadata").children('h1').children('a').text();
+    Playgrub.playlist.set_title('Playlick: '+title);
+    $(".haudio").each(function () {
+        var artist = $(this).children('.contributor').text();
+        var song = $(this).children('.fn').text();
+        Playgrub.playlist.add_track(artist, song);
+    });
+};
+
+Playgrub.source.start();
