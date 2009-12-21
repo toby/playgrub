1ac5bc454f1c4bebb79b2f3dbd531a05c3241cc9
Toby Padilla <tobypadilla@gmail.com>
2009-12-21 14:52:03 -0800

    updated page meta info

--- js/playgrub_bookmarklet.js
+++ js/playgrub_bookmarklet.js
@@ -2,7 +2,7 @@ PlaygrubLoader = {
 
     remotes:  [
         ['jQuery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js'],
-        ['Playgrub', 'http://www.playgrub.com/js/playgrub.js']
+        ['Playgrub', 'http://localhost:8080/js/playgrub.js']
     ],
 
     init: function() {}
