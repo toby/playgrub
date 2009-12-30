7b7cb0dd9e883ec7298d87f09f0daff85d8da89f
jphastings <jphastings@gmail.com>
2009-12-30 02:35:34 +0000

    Bug squash, /radio1/chart/singles wasn't matching

--- scrapers/bbc.co.uk>radio1>chart.js
+++ scrapers/bbc.co.uk>radio1>chart.js
@@ -1,16 +1,16 @@
 /**
  * BBC Radio 1 Charts Scraper
  * Created by: JP Hastings-Spital & Lucas Gonze
- * Version: 0.1
+ * Version: 0.2
  *
  * Notes:
  *
-* To test, go to http://www.bbc.co.uk/radio1/chart/singles
-*/
+ * To test, go to http://www.bbc.co.uk/radio1/chart/singles
+ */
 Playgrub.source.url = 'http://www.bbc.co.uk/radio1/chart/*';
 Playgrub.source.error = 'Oops, make sure its a singles chart!';
 Playgrub.source.scrape = function() {
-	if (/singles\/$/.test(location.href)) {
+	if (/singles\/?$/.test(location.href)) {
  	   $("div.chart li").each(function () {
 		var artist = $(this).find("span.artist").text();
 		var title = $(this).find("span.track").text();
